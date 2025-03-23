347742d5294edb81f9bd7d7e3d73ffe7f4e8764a
peternasc <petodonald@gmail.com>
2025-03-23 03:33:32 +0000

    ajuste da criptografia da senha

--- IaC.sh
+++ IaC.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+#Criação dos Grupos
+
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
+
+#Criação e definoções de permições das pastas
 
 mkdir /publico /adm /ven /sec
 chown root:GRP_ADM /adm
@@ -11,23 +15,25 @@ chown root:GRP_SEC /sec
 chmod 770 /adm /ven /sec
 chmod 777 /publico
 
-useradd carlos -c "Carlos" -s /bin/bash -m -p $(openssl passwd -crypt 123) -G GRP_ADM
+#Criação dos usuarios 
+
+useradd carlos -c "Carlos" -s /bin/bash -m -p $(openssl passwd -6 123) -G GRP_ADM
 passwd carlos -e
-useradd maria -c "Maria" -s /bin/bash -m -p $(openssl passwd -crypt 123) -G GRP_ADM
+useradd maria -c "Maria" -s /bin/bash -m -p $(openssl passwd -6 123) -G GRP_ADM
 passwd maria -e
-useradd joao -c "João" -s /bin/bash -m -p $(openssl passwd -crypt 123) -G GRP_ADM
+useradd joao -c "João" -s /bin/bash -m -p $(openssl passwd -6 123) -G GRP_ADM
 passwd joao -e
-useradd debora -c "Debora" -s /bin/bash -m -p $(openssl passwd -crypt 123) -G GRP_VEN
+useradd debora -c "Debora" -s /bin/bash -m -p $(openssl passwd -6 123) -G GRP_VEN
 passwd debora -e
-useradd sebastiana -c "Sebastiana" -s /bin/bash -m -p $(openssl passwd -crypt 123) -G GRP_VEN
+useradd sebastiana -c "Sebastiana" -s /bin/bash -m -p $(openssl passwd -6 123) -G GRP_VEN
 passwd sebastiana -e
-useradd roberto -c "Roberto" -s /bin/bash -m -p $(openssl passwd -crypt 123) -G GRP_VEN
+useradd roberto -c "Roberto" -s /bin/bash -m -p $(openssl passwd -6 123) -G GRP_VEN
 passwd roberto -e
-useradd josefina -c "Josefina" -s /bin/bash -m -p $(openssl passwd -crypt 123) -G GRP_SEC
+useradd josefina -c "Josefina" -s /bin/bash -m -p $(openssl passwd -6 123) -G GRP_SEC
 passwd josefina -e
-useradd amanda -c "Amanda" -s /bin/bash -m -p $(openssl passwd -crypt 123) -G GRP_SEC
+useradd amanda -c "Amanda" -s /bin/bash -m -p $(openssl passwd -6 123) -G GRP_SEC
 passwd amanda -e
-useradd rogerio -c "Rogerio" -s /bin/bash -m -p $(openssl passwd -crypt 123) -G GRP_SEC
+useradd rogerio -c "Rogerio" -s /bin/bash -m -p $(openssl passwd -6 123) -G GRP_SEC
 passwd rogerio -e
 
 
